b2a4c4862516e4de8ac7d6f7890024b4725ef60a
mellamoZ <zizoxdomi@gmail.com>
2023-02-06 23:13:21 +0300

    lol

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""identify object is exactly an instance of the specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """ identificate class an object"""
+    if type(obj) is a_class:
+        return True
+    return False
